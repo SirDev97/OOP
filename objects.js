4c18814b38b26ac9c1ab430def00ec5a81f8d278
Alkin Maystorov <alkin971@gmail.com>
2021-07-29 11:04:41 +0200

    add Object Literals branch

--- objects.js
+++ objects.js
@@ -1,1 +1,12 @@
+let userOne = {
+  email: "mario@mari.com",
+  name: "mario",
+  login() {
+    console.log(this.email, " has logged in.");
+  },
+  logout() {
+    console.log(this.email, " has logged out.");
+  },
+};
 
+console.log(userOne.name);
