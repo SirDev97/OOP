bf4db63c6a6ac9706357e9587fcf609eb3e927b4
Alkin Maystorov <alkin971@gmail.com>
2021-07-30 20:29:07 +0200

    add JS Classes Branch

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# Updating Properties Branch
+# Classes Branch
+
+- Classes in JavaScript are like blueprints.
+  - example: a User Class will define the structure of all new users and what kind of 'methods' they will have access to.
--- objects.js
+++ objects.js
@@ -1,34 +1,3 @@
-let userOne = {
-  email: "mario@mari.com",
-  name: "mario",
-  login() {
-    console.log(this.email, " has logged in.");
-  },
-  logout() {
-    console.log(this.email, " has logged out.");
-  },
-};
-
-// UPDATING
-userOne.name = "yoshi";
-console.log(userOne.name);
-
-userOne["name"] = "mario";
-console.log(userOne["name"]);
-
-let prop = "name";
-
-userOne[prop];
-console.log(userOne[prop]);
-
-prop = "email";
-console.log(userOne[prop]);
-
-// CREATING
-userOne.age = 25;
-console.log(userOne);
-
-userOne.logInfo = function () {
-  console.log(`${this.name}'s info.`);
-};
-console.log(userOne);
+class User {
+  // Empty Class
+}
